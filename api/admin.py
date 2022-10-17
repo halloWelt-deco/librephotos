c8901047609239919eefcd99a1665b120ab28167
Rohith Palakirti <rohithkp99@gmail.com>
2022-10-17 01:52:36 +0000

    Register TripUser data model

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import AlbumAuto, AlbumUser, Face, Person, Photo
+from .models import AlbumAuto, AlbumUser, Face, Person, Photo, TripUser
 
 # Register your models here.
 
@@ -9,3 +9,4 @@ admin.site.register(Person)
 admin.site.register(Face)
 admin.site.register(AlbumAuto)
 admin.site.register(AlbumUser)
+admin.site.register(TripUser)
